803bdd6af87446180fb716286e6c665009ca95bc
Juan Pablo Blanco <jblanco@despegar.com>
2013-03-05 11:08:50 -0200

    glitch en la matrix

--- js/map.js
+++ js/map.js
@@ -12,36 +12,39 @@ var Map = function(mapData){
 
     var cameraTiles = this.cameraTiles(cameraPosition);
 
-    for(var i=0;i<15;i++){
-      for(var j=0;j<20;j++){
-        sourcePosition = this.tilePositionFromIndex(cameraTiles[i][j]);
-        ctx.drawImage(tileset,sourcePosition.x,sourcePosition.y,sWidth,sHeight,dx,dy,dWidth,dHeight);
-        dx=dx+32;
-      }
+    for(var layerIndex=0;layerIndex<3;layerIndex++){
       dx=0;
-      dy=dy+32;
+      dy=0;
+      for(var i=0;i<15;i++){
+        for(var j=0;j<20;j++){
+          sourcePosition = this.tilePositionFromIndex(cameraTiles[layerIndex][i][j]);
+          ctx.drawImage(tileset,sourcePosition.x,sourcePosition.y,sWidth,sHeight,dx,dy,dWidth,dHeight);
+          dx=dx+32;
+        }
+        dx=0;
+        dy=dy+32;
+      }
     }
-
   };
 
   this.cameraTiles = function(cameraPosition){
     //obtengo los cuadrados del mapa que corresponden a la posicion de la camara
     //y los coloco en una matriz de 20x15
-    var cameraTiles = [[]];
+    var cameraTiles = [[[]]];
     var tileIndex= 0;
     var tileData;
 
-
-    for(var i=0;i<15;i++){
-      cameraTiles[i]=[];
-      for(var j=0;j<20;j++){
-        tileData = this.mapData["layers"][0]["data"][tileIndex];
-        cameraTiles[i][j] = tileData;
-        tileIndex++;
+    for(var layerIndex=0;layerIndex<3;layerIndex++){
+      cameraTiles[layerIndex]=[];
+      for(var i=0;i<15;i++){
+        cameraTiles[layerIndex][i]=[];
+        for(var j=0;j<20;j++){
+          tileData = this.mapData["layers"][layerIndex]["data"][tileIndex];
+          cameraTiles[layerIndex][i][j] = tileData;
+          tileIndex++;
+        }
       }
-
     }
-
     return cameraTiles;
   };
 
